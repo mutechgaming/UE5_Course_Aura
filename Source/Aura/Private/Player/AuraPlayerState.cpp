95dde60e84d851f99e62184060d08b1eada28c28
mutechgaming <mutechgamestudio@gmail.com>
2024-09-21 17:41:14 -0400

    Replication Mode

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -7,11 +7,13 @@
 #include "AbilitySystem/AuraAttributeSet.h"
 
 // netupdatefrequency is how often the server will be updating the clients - 100 is fast, example are games like fortnite
+// init ability actory must be done after the controller has been set to the pawn
 
 AAuraPlayerState::AAuraPlayerState()
 {
 	AbilitySystemComponent = CreateDefaultSubobject <UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 
