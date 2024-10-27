b5e875f328ce70d01086ee32d10d9d4d20526aec
mutechgaming <mutechgamestudio@gmail.com>
2024-10-27 16:23:50 -0400

    Modifier Magnitude Calculations

---
+++ Source/Aura/Public/AbilitySystem/ModMagCalc/MMC_MaxHealth.h
@@ -0,0 +1,26 @@
+// Copyrighte Steven Mutek
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_MaxHealth.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UMMC_MaxHealth : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+	
+public:
+
+	UMMC_MaxHealth(); // constructor
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+
+private:
+	
+	FGameplayEffectAttributeCaptureDefinition VigorDef;
+};
